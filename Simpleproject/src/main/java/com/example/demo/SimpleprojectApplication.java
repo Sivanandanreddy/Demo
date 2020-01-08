5ad6080571d2c143c5764a1bbc75d42dae84365d
SivanandanReddy <SivanandanReddy@DESKTOP-7DAFA8V.in.ibm.com>
2020-01-08 16:23:22 +0530

    added component

--- Simpleproject/src/main/java/com/example/demo/SimpleprojectApplication.java
+++ Simpleproject/src/main/java/com/example/demo/SimpleprojectApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @RestControllerAdvice
+@Component
 public class SimpleprojectApplication {
 
 	public static void main(String[] args) {
