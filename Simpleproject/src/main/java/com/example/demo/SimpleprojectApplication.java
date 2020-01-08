94a1dd3681b1149af54df752f42dca6569e98e9d
SivanandanReddy <SivanandanReddy@DESKTOP-7DAFA8V.in.ibm.com>
2020-01-08 16:36:48 +0530

    resloved conflicts on method level

--- Simpleproject/src/main/java/com/example/demo/SimpleprojectApplication.java
+++ Simpleproject/src/main/java/com/example/demo/SimpleprojectApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +20,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class SimpleprojectApplication {
 
 	@ExceptionHandler
+
+	@RequestMapping
+
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleprojectApplication.class, args);
 	}
