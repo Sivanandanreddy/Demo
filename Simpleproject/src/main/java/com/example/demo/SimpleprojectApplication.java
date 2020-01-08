416fca8949c2191dded9b7f28336c3127f9d61a6
SivanandanReddy <SivanandanReddy@DESKTOP-7DAFA8V.in.ibm.com>
2020-01-08 16:34:10 +0530

    added exception controller

--- Simpleproject/src/main/java/com/example/demo/SimpleprojectApplication.java
+++ Simpleproject/src/main/java/com/example/demo/SimpleprojectApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestController
 public class SimpleprojectApplication {
 
+	@ExceptionHandler
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleprojectApplication.class, args);
 	}
